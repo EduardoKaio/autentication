67dd31ab75720deb1015557d887a1f1181de569e
kaio <kaioeduardooficial@gmail.com>
2023-06-22 23:41:51 -0300

    mudacas

--- login/settings.py
+++ login/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@o2jgpfua#ony$k4-qbaqvelt+1ji7sy!*p(_lc(os%_kq@so5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
