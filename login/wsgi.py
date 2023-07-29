6e638eadf23d281484f7fdc6dea58532644c66e9
kaio <kaioeduardooficial@gmail.com>
2023-07-29 20:28:00 -0300

    mod base

--- login/wsgi.py
+++ login/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'login.settings')
 
-application = get_wsgi_application()
-app = application
+app = get_wsgi_application()
+
 
 
