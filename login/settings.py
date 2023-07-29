b83f6a4d709d89eabf9c90271714c4eefd5cec71
kaio <kaioeduardooficial@gmail.com>
2023-07-29 19:05:41 -0300

    debug false

--- login/settings.py
+++ login/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@o2jgpfua#ony$k4-qbaqvelt+1ji7sy!*p(_lc(os%_kq@so5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
