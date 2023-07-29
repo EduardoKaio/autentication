6e072fdea4aacb9c3e37f3c663811695d2a4d036
kaio <kaioeduardooficial@gmail.com>
2023-07-29 20:40:54 -0300

    settings mod

--- login/settings.py
+++ login/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DATABASE_URL="postgresql://postgres:sGv25JQp6yYw0h9FSqhB@containers-us-west-150.railway.app:6097/railway"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -77,13 +77,15 @@ WSGI_APPLICATION = 'login.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+"default":dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
-DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
